61e017334bf56f2b51625178fcc002f0e99722c7
Jarosław Weselski <jweselski@objectivity.co.uk>
2019-02-06 12:15:16 +0100

    Add filter to enable only SSL requests

--- VidlyApp/App_Start/FilterConfig.cs
+++ VidlyApp/App_Start/FilterConfig.cs
@@ -9,6 +9,7 @@ namespace VidlyApp
         {
             filters.Add(new HandleErrorAttribute());
             filters.Add(new AuthorizeAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
