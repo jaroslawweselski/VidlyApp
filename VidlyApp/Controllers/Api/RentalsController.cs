44d804d876086fdbe00af9ebed4983d4e76fd4cf
Jarosław Weselski <jweselski@objectivity.co.uk>
2019-02-06 15:32:04 +0100

    Add NewRental

---
+++ VidlyApp/Controllers/Api/RentalsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VidlyApp.Dtos;
+
+namespace VidlyApp.Controllers.Api
+{
+    public class RentalsController : ApiController
+    {
+        // POST /api/rentals
+        [HttpPost]
+        public IHttpActionResult CreateNewRental(NewRentalDto NewRental) 
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VidlyApp/Models/Rental.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VidlyApp.Models
+{
+    public class Rental
+    {
+        public DateTime DateRented { get; set; }
+
+        public DateTime DateReturned { get; set; }
+    }
+}
