34a41758a8df024586d07b4fe173371405ba5c6d
Jarosław Weselski <jweselski@objectivity.co.uk>
2019-02-07 11:15:52 +0100

    Update Rental Model and Migrations

--- VidlyApp/Models/Rental.cs
+++ VidlyApp/Models/Rental.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +10,14 @@ namespace VidlyApp.Models
     {
         public int Id { get; set; }
 
+        [Required]
+        public Customer Customer { get; set; }
+
+        [Required]
+        public Movie Movie { get; set; }
+
         public DateTime DateRented { get; set; }
 
-        public DateTime DateReturned { get; set; }
+        public DateTime? DateReturned { get; set; }
     }
 }
