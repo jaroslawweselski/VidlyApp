e3cc08faa4e6aa3efaf5672e34b473c2a1eb28b9
Jarosław Weselski <jweselski@objectivity.co.uk>
2019-02-07 15:52:10 +0100

    New Rental Form

--- VidlyApp/Controllers/Api/RentalsController.cs
+++ VidlyApp/Controllers/Api/RentalsController.cs
@@ -23,11 +23,17 @@ namespace VidlyApp.Controllers.Api
         [HttpPost]
         public IHttpActionResult CreateNewRental(NewRentalDto newRentalDto) 
         {
-            var customer = _context.Customers.Single(c => c.Id == newRentalDto.CustomerId);
-            var movies = _context.Movies.Where(m => newRentalDto.MovieIds.Contains(m.Id));
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == newRentalDto.CustomerId);          
+        
+            var movies = _context.Movies.Where(m => newRentalDto.MovieIds.Contains(m.Id)).ToList();
 
             foreach (var movie in movies)
             {
+                if (movie.NumberAvailable == 0)
+                {
+                    return BadRequest("Movie is not available.");
+                }
+
                 movie.NumberAvailable--; 
 
                 var rental = new Rental
